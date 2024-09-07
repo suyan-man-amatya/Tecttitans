2c560e6d34272bc4f2175ee135b3efb2a9dda946
suyan-man-amatya <suyanmanamatya8@gmail.com>
2024-09-07 14:53:35 +0545

    Update Footer.jsx

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,25 +1,57 @@
 import React from "react";
+import Logo from "../assets/web-logo2.png";
+import DcAvatar from "../assets/footerDoc.png";
+import { FaRegCopyright } from "react-icons/fa6";
 
 function Footer() {
   return (
     <div>
-      <div className="upper-footer">
-        <div className="logo-sec">
-          <img src="" alt="" />
-        </div>
-        <div className="text-sec">
-          <div className="First-col">
-            <div>Home</div>
-            <div>About</div>
-            <div>Care Services</div>
+      <div className="upper-footer flex justify-between h-2/4 px-40">
+        <div className="logo-section  flex flex-col items-center justify-center ">
+          <div className="h-40 w-40">
+            {" "}
+            <img src={Logo} alt="" />
           </div>
-          <div className="second-col">
-            <div>Patient</div>
-            <div>Doctor</div>
+          <div className="text text-2xl text-green-500 font-medium">
+            Let's Dive into the World.
+          </div>
+        </div>
+        <div className="text-section flex flex-row items-center text-lg font-normal">
+          <div className="row px-32">
+            <div className="hover:text-green-500 hover:underline hover:cursor-pointer">
+              Home
+            </div>
+            <div className="py-4 hover:text-green-500 hover:underline hover:cursor-pointer">
+              About
+            </div>
+            <div className="hover:text-green-500 hover:underline hover:cursor-pointer">
+              Care services
+            </div>
+          </div>
+
+          <div className="doc w-32">
+            <img src={DcAvatar} alt="" />
           </div>
         </div>
       </div>
-      <div className="lower-footer"></div>
+      <div className="px-16 py-1">
+        <hr />
+      </div>
+      <div className="lower-footer flex justify-between px-24">
+        <div className="copyright flex items-center">
+          <div className="copyright-logo">
+            <FaRegCopyright />
+          </div>
+          <div className="copyright-text font-normal">
+            ,All rights reserved.
+          </div>
+        </div>
+        <div className="tandcs flex font-normal gap-8">
+          <div>Terms and services</div>
+          <div>Privacy Policies</div>
+          <div>Cookies</div>
+        </div>
+      </div>
     </div>
   );
 }
